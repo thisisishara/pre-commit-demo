15e8d37a4b94d7c484c72fc626abf96925712954
Ishara Dissanayake <thisismaduishara@gmail.com>
2023-04-15 13:16:29 +0530

    testing pre-commit hooks

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from datetime import datetime
+from typing import Text
 
 
-def get_date_from_string(date_str, format_):
+def get_date_from_string(date_str: Text, format_: Text) -> datetime:
 
     return datetime.strptime(date_str, format_)
 
 
 if __name__ == "__main__":
+
     date_ = "2022.02.21"
     format_ = "%Y.%m.%d"
     print(f"main is running and the date is {get_date_from_string(date_, format_)}...")
