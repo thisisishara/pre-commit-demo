bdf7227d8b2d6537dd690bb03fda7d183e753e10
Ishara Dissanayake <thisismaduishara@gmail.com>
2023-04-15 13:35:39 +0530

    testing pre-commit hooks

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 from datetime import datetime
-from typing import Text
+from typing import Any, Text, List
 
 
 def get_date_from_string(date_str: Text, format_: Text) -> datetime:
-
     return datetime.strptime(date_str, format_)
 
 
+def add_numbers(numbers: List[int]) -> Any:
+    return sum(numbers)
+
+
 if __name__ == "__main__":
-
-    def add_numbers(numbers):
-        return sum(numbers)
-
     print(add_numbers([1, 2, 3]))
-
     date_ = "2022.02.21"
     format_ = "%Y.%m.%d"
     print(f"main is running and the date is {get_date_from_string(date_, format_)}...")
