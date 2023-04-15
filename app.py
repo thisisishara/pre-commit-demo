9a84a8a83aacf648461137fed9de63be41d48afb
Ishara Dissanayake <thisismaduishara@gmail.com>
2023-04-15 13:05:35 +0530

    edited pre-commit hooks

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-import os
-
-
-import hashlib
-
-
 from datetime import datetime
 
 
+def get_date_from_string(date_str, format_):
+
+    return datetime.strptime(date_str, format_)
+
+
 if __name__ == "__main__":
-    print(f"main is running...")
+    date_ = "2022.02.21"
+    format_ = "%Y.%m.%d"
+    print(f"main is running and the date is {get_date_from_string(date_, format_)}...")
